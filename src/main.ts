f5dac69d020a76e7bdcd98e57191db35085ecfda
Franklin Shera <fshera96@gmail.com>
2021-12-25 02:57:16 +0300

    HEROKU

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{ cors: true });
-  await app.listen(3000,() => console.log("Nestjs Running at http://localhost:3000")
-  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
