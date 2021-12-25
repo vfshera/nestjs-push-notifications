a8f59a6d62abcb5a724e723d63e9e9dcb4f00f20
Franklin Shera <fshera96@gmail.com>
2021-12-25 03:54:49 +0300

    HEROKU

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,15 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         // type: 'mysql',
-        url: process.env.DB_URL,
         type: 'postgres',
         ssl:{
           rejectUnauthorized: false
         },
         host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT) || 3306,
-        // username: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false,
         autoLoadEntities: true
